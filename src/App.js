dfdf2114e3062ba99cf61c36f745a0763b57fdd6
Gulam Anas <g.anas786@gmail.com>
2024-12-15 23:41:27 +0530

    added redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Cards from "./components/Cards";
 
 function App() {
   
   return (
     <div className="App">
-      <Cards/>
+      <p>Hello world!</p>
     </div>
   );
 }
